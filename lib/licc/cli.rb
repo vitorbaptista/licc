98f75b8c814ed618d5c635328657351336094cd0
Vítor Baptista <vitor@vitorbaptista.com>
2010-09-23 16:59:12 -0300

    Prints info about the licenses received as argument.

--- lib/licc/cli.rb
+++ lib/licc/cli.rb
@@ -1,6 +1,14 @@
+require 'licc/license'
+
 module Licc
     class CLI
+        LICENSES_FOLDER = File.dirname(__FILE__) + '/licenses/'
+
         def self.execute(stdout=STDOUT, stdin=STDIN, arguments=[])
+            arguments.each { |license|
+                license_path = LICENSES_FOLDER + license.downcase + '.rdf'
+                puts License.new(license_path) if File.exists? license_path
+            }
         end
     end
 end
