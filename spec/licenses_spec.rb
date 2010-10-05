600665a0f3af4cc0920dfbe46fbc2eef2a7a92ee
Vítor Baptista <vitor@vitorbaptista.com>
2010-10-05 17:18:17 -0300

    Adds license combinability rules and tests.

--- spec/licenses_spec.rb
+++ spec/licenses_spec.rb
@@ -5,10 +5,12 @@ require 'licc/licenses'
 describe Licc::Licenses do
   before(:all) do
     permissions = ['DerivativeWorks', 'Distribution', 'Reproduction']
-    requirements = ['Copyleft', 'Notice', 'SourceCode']
-    @gpl = Licc::License.new('GPL', '3.0', permissions, requirements, [])
+    gpl_requirements = ['Copyleft', 'Notice', 'SourceCode']
+    by_sa_requirements = ['Attribution', 'Notice', 'ShareAlike']
+    @gpl = Licc::License.new('GPL', '3.0', permissions, gpl_requirements, [])
     @bsd = Licc::License.new('BSD', '', permissions, ['Notice'], [])
     @by  = Licc::License.new('BY', '3.0', permissions, ['Attribution', 'Notice'], [])
+    @by_sa  = Licc::License.new('BY-SA', '3.0', permissions, by_sa_requirements, [])
   end
 
   it "should accept an array of licenses" do
@@ -57,4 +59,15 @@ describe Licc::Licenses do
     l.licenses.should == licenses + [@by]
   end
 
+  it "should detect known combinable license as such" do
+    licenses = [@gpl, @bsd, @by]
+    l = Licc::Licenses.new(licenses)
+    l.combinable?.should == true
+  end
+
+  it "should detect known non-combinable license as such" do
+    licenses = [@gpl, @bsd, @by_sa]
+    l = Licc::Licenses.new(licenses)
+    l.combinable?.should == false
+  end
 end
