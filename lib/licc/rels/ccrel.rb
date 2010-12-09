02b640602506dd25204fdd031b1dbe0a7590100c
Vítor Baptista <vitor@vitorbaptista.com>
2010-12-09 20:31:56 -0300

    Everything relative to the parse method should be inside it.

--- lib/licc/rels/ccrel.rb
+++ lib/licc/rels/ccrel.rb
@@ -6,15 +6,17 @@ module Licc
         class CCREL
             attr_reader :identifier, :version, :permits, :requires, :prohibits
 
-            CC = 'http://creativecommons.org/ns#'
-            CC_PERMITS = CC + 'permits'
-            CC_REQUIRES = CC + 'requires'
-            CC_PROHIBITS = CC + 'prohibits'
-
-            DC = 'http://purl.org/dc/elements/1.1/'
-            DCQ = 'http://purl.org/dc/terms/'
-
             def self.parse(rdf_uri)
+                # RDF predicates used by ccREL.
+                cc = 'http://creativecommons.org/ns#'
+                cc_permits = cc + 'permits'
+                cc_requires = cc + 'requires'
+                cc_prohibits = cc + 'prohibits'
+
+                dc = 'http://purl.org/dc/elements/1.1/'
+                dcq = 'http://purl.org/dc/terms/'
+
+                # Initializing variables that will hold the results.
                 permits = []
                 requires = []
                 prohibits = []
@@ -23,17 +25,17 @@ module Licc
 
                 RDF::Reader.open(rdf_uri) do |reader|
                     reader.each_statement do |s|
-                        object = s.object.to_s.gsub(CC, '').gsub(DC, '')
+                        object = s.object.to_s.gsub(cc, '').gsub(dc, '')
                         predicate = s.predicate.to_s
 
-                        # CC's licenses use DC, FSF's use DCQ. We test both.
-                        identifier = object if predicate == DC + 'identifier'
-                        identifier = object if predicate == DCQ + 'identifier'
+                        # CC's licenses use DC, FSF's use dcQ. We test both.
+                        identifier = object if predicate == dc + 'identifier'
+                        identifier = object if predicate == dcq + 'identifier'
 
-                        version = object    if predicate == DCQ + 'hasVersion'
-                        permits << object   if predicate == CC_PERMITS
-                        requires << object  if predicate == CC_REQUIRES
-                        prohibits << object if predicate == CC_PROHIBITS
+                        version = object    if predicate == dcq + 'hasVersion'
+                        permits << object   if predicate == cc_permits
+                        requires << object  if predicate == cc_requires
+                        prohibits << object if predicate == cc_prohibits
                     end
                 end
 
